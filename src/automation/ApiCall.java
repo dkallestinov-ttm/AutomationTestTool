437c0f11695160d2f6ae3af541917eccaf83d769
Brian Lueck <brianlueck@iseinc.biz>
2018-07-30 10:23:56 -0500

    Add old src.

---
+++ src/automation/ApiCall.java
@@ -0,0 +1,155 @@
+package automation;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import com.squareup.okhttp.*;
+
+public class ApiCall {
+    private static OkHttpClient client = new OkHttpClient();
+    private static final String DATEFORMAT = "yyyy-MM-dd";
+    private static String CURRENTDIRECTORYFILEPATH = System.getProperty("user.dir");
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String BASICAUTH = "Basic b21uaUFkYXB0ZXI6azN5U2l4cmlkMW9n";
+    private static final String VALKYRIEQAURL = "https://valkyrie.qa.connectedfleet.io";
+    private static final String APPLICATIONJSON = "application/json";
+    private static final String  CACHECONTROL = "Cache-Control";
+    private static final String CONTENTTYPE = "Content-Type";
+    private static final String NOCACHE = "no-cache";
+    private static String setCurrentDateAsScheduledDate = getUTCdatetimeAsString();
+
+    //Date in UTC because Valkyrie needs UTC time to schedule an OTAP
+    private static String getUTCdatetimeAsString() {
+        final SimpleDateFormat sdf = new SimpleDateFormat(DATEFORMAT);
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        final String utcTime = sdf.format(new Date());
+        return (utcTime+"T00:00:00Z");
+    }
+
+    //Scheduling Multiple DSNs
+    private String submitBatch(String dsn) throws IOException {
+        String setCurrentDateAsScheduledDate = getUTCdatetimeAsString();
+        String JSONToPost = "{\r\n  \"submittedBy\": \"Aditya Test via Java\",\r\n  \"scheduledDate\": \""+setCurrentDateAsScheduledDate+"\",\r\n  \"dsns\": [\r\n    "+dsn+"\r\n  ],\r\n  \"profile\": {\r\n    \"id\": 650448,\r\n    \"name\": \"ASTest1\"\r\n  },\r\n  \"name\": \"API_Tool\"\r\n}";
+
+        // POST Request to SUBMIT a batch to Valkyrie
+        MediaType mediaType = MediaType.parse(APPLICATIONJSON);
+        RequestBody body = RequestBody.create(mediaType, JSONToPost);
+        Request request = new Request.Builder()
+                .url(VALKYRIEQAURL+"/submitBatch")
+                .post(body)
+                .addHeader(CONTENTTYPE, APPLICATIONJSON)
+                .addHeader(CACHECONTROL, NOCACHE)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.print("submitBatch: "+response.code());
+        return response.body().string();
+    }
+
+    //Scheduling Single DSN
+    private String submitBatch(int dsn) throws IOException {
+        String setCurrentDateAsScheduledDate = getUTCdatetimeAsString();
+        String JSONToPost = "{\r\n  \"submittedBy\": \"Aditya Test via Java\",\r\n  \"scheduledDate\": \""+setCurrentDateAsScheduledDate+"\",\r\n  \"dsns\": [\r\n    "+dsn+"\r\n  ],\r\n  \"profile\": {\r\n    \"id\": 650448,\r\n    \"name\": \"ASTest1\"\r\n  },\r\n  \"name\": \"API_Tool\"\r\n}";
+
+        // POST Request to SUBMIT a batch to Valkyrie
+        MediaType mediaType = MediaType.parse(APPLICATIONJSON);
+        RequestBody body = RequestBody.create(mediaType, JSONToPost);
+        Request request = new Request.Builder()
+                .url(VALKYRIEQAURL+"/submitBatch")
+                .post(body)
+                .addHeader(CONTENTTYPE, APPLICATIONJSON)
+                .addHeader(CACHECONTROL, NOCACHE)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.print("submitBatch: "+response.code());
+        return response.body().string();
+    }
+
+    //Scheduling Multiple DSNs
+    public static String submitAndApproveBatch(String dsn, String profileID) throws IOException {
+
+        String jsonStringSubmitBatch = "{\n" +
+                "  \"submittedBy\": \"TestTool\",\n" +
+                "  \"scheduledDate\": \"" + setCurrentDateAsScheduledDate + "\",\n" +
+                "  \"dsns\": [\n" +
+                "    "+ dsn +"\n" +
+                "  ],\n" +
+                "  \"profile\": {\n" +
+                "    \"id\": "+ profileID + "\n" +
+                "  },\n" +
+                "  \"name\": \"Batch1\"\n" +
+                "}";
+
+        MediaType mediaType = MediaType.parse(APPLICATIONJSON);
+        RequestBody body = RequestBody.create(mediaType, jsonStringSubmitBatch);
+        Request request = new Request.Builder()
+                .url(VALKYRIEQAURL+"/submitAndApproveBatch")
+                .post(body)
+                .addHeader(CONTENTTYPE, APPLICATIONJSON)
+                .addHeader(CACHECONTROL, NOCACHE)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String responseString = response.body().string();
+        System.out.print("submitBatch Response Body : " + responseString + " submitBatch ResponseCode : " + response.code() + " ");
+        return responseString;
+    }
+
+    public int nextPackages(int dsn) throws IOException {
+        String nextPackagesURLString = VALKYRIEQAURL+"/nextPackages/"+dsn;
+
+        // GET Request for nextPackages endpoint to Valkyrie
+        Request request = new Request.Builder()
+                .url(nextPackagesURLString)
+                .get()
+                .addHeader(AUTHORIZATION, BASICAUTH)
+                .addHeader(CACHECONTROL, NOCACHE)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.print("nextPackages: "+ response.code() +" ");
+        //return response.body().string();
+        return response.code();
+    }
+
+    public int packagesReady(int dsn) throws IOException {
+        String packagesReadyURLString=VALKYRIEQAURL+"/packagesReady/p.mtp."+dsn+".otap.module-ready";
+
+        // PUT Request for nextPackages endpoint to Valkyrie
+        Request request = new Request.Builder()
+                .url(packagesReadyURLString)
+                .put(null)
+                .addHeader(AUTHORIZATION, BASICAUTH)
+                .addHeader(CACHECONTROL, NOCACHE)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.print("packagesReady: "+response.code() +" ");
+        //return response.body().string();
+        return response.code();
+    }
+
+    public int otapCompleted(int dsn, String profilename) throws IOException {
+        String pathString = CURRENTDIRECTORYFILEPATH +File.separator+"Binaries"+File.separator+dsn+"_"+profilename+".dat";
+        String otapCompletedURLString=VALKYRIEQAURL+"/otapCompleted/p.mop."+dsn+".210.icap-rom-version";
+
+        // POST Request for nextPackages endpoint to Valkyrie
+        MediaType mediaType = MediaType.parse("application/octet-stream");
+        Request request = new Request.Builder()
+                .url(otapCompletedURLString)
+                .post(RequestBody.create(mediaType, Files.readAllBytes(Paths.get(pathString))))
+                .addHeader(AUTHORIZATION, BASICAUTH)
+                .addHeader(CACHECONTROL, NOCACHE)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.print("otapCompleted: " + response.code() + " ");
+        //return response.body().string();
+        return response.code();
+    }
+}
---
+++ src/automation/ValkyrieSQL.java
@@ -0,0 +1,104 @@
+package automation;
+
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class ValkyrieSQL {
+    private static final String SQLCONNECTIONURL = "jdbc:mysql://qa-security-user.ckcpzgmqvbbt.us-east-1.rds.amazonaws.com";
+    private static final String USERNAME = "valkyrie_db";
+    private static final String PASSWORD = "valkyrie_db";
+    private static final String JDBCDRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final int updateTypeIdIndex = 1;
+    private static final int versionIndex = 2;
+    private static final int dependentUpdateTypeIdIndex = 3;
+    private static final int dependentVersionIndex = 4;
+    private static String sqlQueryForGettingUpdateTypeIdAndTargetVersion = "SELECT\n" +
+            "pkg.update_type_id,\n" +
+            "pkg.version,\n" +
+            "pkg_dep.update_type_id AS dependentUpdateTypeId,\n" +
+            "pkg_dep.version AS dependentVersion\n" +
+            "FROM valkyrie_db.profile p\n" +
+            "JOIN valkyrie_db.profile_package pp ON pp.profile_id=p.id\n" +
+            "JOIN valkyrie_db.package pkg ON pkg.id=pp.package_id\n" +
+            "LEFT JOIN valkyrie_db.package_dependency pd ON pd.package_id=pp.package_id\n" +
+            "LEFT JOIN valkyrie_db.package pkg_dep ON pkg_dep.id=pd.dependent_on_package_id\n" +
+            "WHERE p.id=";
+
+    //returns Profile Name associated with the ProfileID
+    public static String getProfileNameFromTable(int profileID) {
+        String sqlQuery = "SELECT name FROM valkyrie_db.profile WHERE id="+profileID+";";
+        String profileName="";
+        try{
+            Class.forName(JDBCDRIVER);
+            Connection connection = DriverManager.getConnection(
+                    SQLCONNECTIONURL,
+                    USERNAME,
+                    PASSWORD
+            );
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+            int columnNumber = resultSetMetaData.getColumnCount();
+            while(resultSet.next()) {
+                profileName = resultSet.getString(columnNumber);
+            }
+            connection.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return profileName;
+    }
+
+
+    //returns Profile Name associated with the ProfileID
+    public static Map getAllUpdateTypeIdsAndVersions(int profileID) {
+        sqlQueryForGettingUpdateTypeIdAndTargetVersion = sqlQueryForGettingUpdateTypeIdAndTargetVersion + profileID + ";";
+        Map<Integer,Integer> map = new HashMap<>();
+        try{
+            Class.forName(JDBCDRIVER);
+            Connection connection = DriverManager.getConnection(
+                    SQLCONNECTIONURL,
+                    USERNAME,
+                    PASSWORD
+            );
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sqlQueryForGettingUpdateTypeIdAndTargetVersion);
+            while(resultSet.next()) {
+                Integer updateTypeId = resultSet.getInt(updateTypeIdIndex);
+                Integer version = resultSet.getInt(versionIndex);
+                Integer dependentUpdateTypeId = resultSet.getInt(dependentUpdateTypeIdIndex);
+                Integer dependentVersion = resultSet.getInt(dependentVersionIndex);
+                map.putIfAbsent(updateTypeId,version);
+                map.putIfAbsent(dependentUpdateTypeId,dependentVersion);
+            }
+            map.remove(0);
+            connection.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+    public static String convertHashMapToString(Map map){
+        StringBuilder keyValuePair = new StringBuilder();
+        Iterator keyIterator = map.keySet().iterator();
+        Iterator valueIterator = map.values().iterator();
+        while (keyIterator.hasNext() && valueIterator.hasNext()){
+            int key = (int) keyIterator.next();
+            int value = (int) valueIterator.next();
+            keyValuePair.append(key);
+            keyValuePair.append(" ");
+            keyValuePair.append(value);
+            keyValuePair.append(" ");
+        }
+        return keyValuePair.toString().trim();
+    }
+
+    public static String execute(int profileID) {
+        Map<Integer,Integer> map = getAllUpdateTypeIdsAndVersions(profileID);
+        String updateTypeIdsTargetVersions = convertHashMapToString(map);
+        return updateTypeIdsTargetVersions;
+    }
+}
